b819d8a4bd79412161b8a7dafdb3d8d6a2b6c27c
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-08 19:14:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	getMakeModel()=>`${this.make} this is make and ${this.model} this is model`
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	super(make,model);
+	getTopSpeed()=>this.topSpeed;
+}
 
 // Do not change the code below
 window.Car = Car;
