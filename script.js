816705ada5c08ab980007130b6da230c5d1434e6
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-04-30 17:58:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 // Complete the js code
 function Car(make, model) {
+	this.make=make;
+	this.model=model;
 	getMakeModel()=>`${this.make} this is make and ${this.model} this is model`
 }
+Car.prototype.getMakeModel=function(){return `${this.make} this is make and ${this.model} this is model`}
 
 function SportsCar(make, model, topSpeed) {
-	super(make,model);
-	getTopSpeed()=>this.topSpeed;
+	super(this,make,model);
+	this.topSpeed=topSpeed;
 }
 
+SportsCar.prototype=Object.create(Car.prototype);
+SportsCar.prototype.constructor=SportsCar.prototype;
+SportsCar.prototype=function(){
+	return this.topSpeed;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
